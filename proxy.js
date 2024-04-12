85b9321d8fb5de5c47501469e397db9a2b3ecaa2
GalacticUnblockerOfficial <160625207+GalacticUnblockingCO@users.noreply.github.com>
2024-04-12 10:59:05 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,14 +1,29 @@
 document.getElementById('search-form').addEventListener('submit', function(event) {
     event.preventDefault();
     var query = document.getElementById('search-input').value.trim();
-    if (query !== '') {
+    if (isValidURL(query)) {
+        // If the input is a valid URL, redirect the user to that URL
+        window.location.href = query;
+    } else if (query !== '') {
+        // If the input is not a valid URL, perform a search using the custom search engine
         search(query);
     }
 });
 
+function isValidURL(url) {
+    // Regular expression to check if the input string is a valid URL
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return pattern.test(url);
+}
+
 function search(query) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://www.google.com/search?q=' + encodeURIComponent(query), true);
+    xhr.open('GET', 'https://cse.google.com/cse?cx=50321224bee844598#gsc.tab=0' + encodeURIComponent(query), true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
